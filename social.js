2bb92047190fdabf8c5945bddf8cb82cfa325f45
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2024-01-03 19:43:20 -0300

    first update of 2024
    in this month, i begin the fir atualization of my portfifolio with new ideas, to make it more objetive and nice to see

---
+++ social.js
@@ -0,0 +1,3 @@
+const name = prompt('Yoo! who are you? | Olá, como se chama? ')
+        alert(` Welcome ${name}. Meet me!   |  Seja bem vindo ${name}! Vem me conhecer! `)
+        alert('Do you prefer e-mail? Send me in ysogeek@hotmail.com or ymenephyl@gmail.com  |  meus endereços de e-mail.')
